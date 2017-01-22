687c2adf86f2affc69a0431163e22d8803f7d36c
alephspb <alephspb@gmail.com>
2017-01-21 21:07:27 -0500

    update

---
+++ src/test/java/com/jukta/base/TestPageStarter.java
@@ -0,0 +1,18 @@
+package com.jukta.base;
+
+import com.jukta.jtahoe.dev.Dev;
+
+import java.io.IOException;
+
+/**
+ * @since 1.0
+ */
+public class TestPageStarter {
+
+    public static void main(String[] args) throws IOException {
+        Dev.main(new String[0]);
+        System.out.println("http://localhost:8080/test.LayoutTestPage");
+        System.out.println("http://localhost:8080/test.FormTestPage");
+    }
+
+}
